4edc2d2e894e7fea4916b7e527e5e61e68708dd5
21310174 <21310174@ppti-24-304-14.ufr-info-p6.jussieu.fr>
2023-10-04 16:25:01 +0200

    nez

--- TME2/main3.cpp
+++ TME2/main3.cpp
@@ -15,7 +15,7 @@ int main () {
 	cout << "Parsing War and Peace" << endl;
 
 	size_t nombre_lu = 0;
-	// prochain mot lu
+	// prochain mot lufsffs
 	string word;
 	// une regex qui reconnait les caractères anormaux (négation des lettres)
 	regex re( R"([^a-zA-Z])");
