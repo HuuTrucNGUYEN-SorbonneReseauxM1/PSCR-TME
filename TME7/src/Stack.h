848e14b563ad14b3168415819910b66f78baf7ca
HuuTrucNGUYEN-SorbonneReseauxM1 <145580726+HuuTrucNGUYEN-SorbonneReseauxM1@users.noreply.github.com>
2023-12-11 17:07:11 +0100

    Update Stack.h

--- TME7/src/Stack.h
+++ TME7/src/Stack.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <sys/sem.h>
+#include <unistd.h>
 #include <cstring> // size_t,memset
 
 namespace pr {
@@ -10,18 +11,46 @@ template<typename T>
 class Stack {
 	T tab [STACKSIZE];
 	size_t sz;
+	int semid;
+	sembuf p_lock[2], v_unlock[2]
 public :
-	Stack () : sz(0) { memset(tab,0,sizeof tab) ;}
+	Stack () : sz(0) { 
+		memset(tab,0,sizeof tab) ;
+		semid = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666);
+        	semctl(semid, 0, SETVAL, 1);
+        	semctl(semid, 1, SETVAL, STACKSIZE); 
+
+        	p_lock[0] = {0, -1, 0}; //lock	
+        	p_lock[1] = {1, -1, 0}; //decrease
+        	v_unlock[0] = {0, 1, 0}; //unlock
+        	v_unlock[1] = {1, 1, 0}; //increase
+	}
 
 	T pop () {
 		// bloquer si vide
-		T toret = tab[--sz];
-		return toret;
+		semop(semid, &p_lock[0], 1);
+        	T toret = tab[--sz];
+       		semop(semid, v_unlock, 2); 
+        	return toret;
 	}
 
 	void push(T elt) {
 		//bloquer si plein
-		tab[sz++] = elt;
+		semop(semid, p_lock, 2); 
+        	tab[sz++] = elt;
+        	semop(semid, &v_unlock[0], 1);
+	}
+
+ 	~Stack() {
+        	semctl(semid, 0, IPC_RMID);
+    	}
+
+	size_t getSz(){
+		return sz;
+	}
+
+	size_t getSzMax(){
+		return STACKSIZE;
 	}
 };
 
